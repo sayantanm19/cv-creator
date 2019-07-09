66ca06fefbd65784dca7fba7887bcec5216e37e8
sayantanm19 <sayantanmaiti@gmail.com>
2019-07-09 14:07:13 +0530

    Changed Styling

--- js/main.js
+++ js/main.js
@@ -25,7 +25,7 @@ function getSkills() {
     // convert NodeList to array for using map function
     let skills = Array.from(rawSkills)
         .map(elem => elem.value)
-        .filter(elem => elem!='');
+        .filter(elem => elem != '');
 
     console.log('Skills: ', skills);
 
@@ -71,21 +71,17 @@ function getWorks() {
         date_to = elem.children[2].children[0].value;
 
         if (company.trim() != '') {
-            expObj = {company, date_from, date_to};
+            expObj = { company, date_from, date_to };
             expList.push(expObj);
         }
     });
 
     console.log('Exp List:', expList);
-    
+
     return expList;
 }
 
-function makeCV() {
-    // Set CV portion visible
-    document.querySelector('.cv-output').style.display = 'flex';
-    document.querySelector('.generator-module').style.display = 'none';
-
+function packData() {
     name = document.querySelector('#nameField').value;
     loc = document.querySelector('#locField').value;
     curr_work = document.querySelector('#workField').value;
@@ -100,23 +96,39 @@ function makeCV() {
 
     workExpListHtml = `<ul>`;
     experiences.forEach(exp => {
-        workExpListHtml += `<li>Worked At: ${exp.company} From: ${exp.date_from} To: ${exp.date_to}</li>`;
+        workExpListHtml += `<li>${exp.company} <b>From:</b> ${exp.date_from} <b>To:</b> ${exp.date_to}</li>`;
     });
     workExpListHtml += `</li>`;
 
-    document.querySelector('#final-name').textContent = name;
-    document.querySelector('#final-location').textContent = loc;
-    document.querySelector('#final-work').textContent = curr_work;
-    document.querySelector('#final-skills').innerHTML = skillsListHtml;
-    document.querySelector('#final-workexp').innerHTML = workExpListHtml;
-    
+    cv_data = { name, loc, curr_work, skillsListHtml, workExpListHtml };
+
+    return cv_data;
+}
+
+function makeCV() {
+    // Set CV portion visible
+    document.querySelector('.cv-output').style.display = 'flex';
+    document.querySelector('.generator-module').style.display = 'none';
+
+    outputData = packData();
+    console.log(outputData);
+
+    document.querySelector('#final-name').textContent = outputData.name;
+    document.querySelector('#final-location').textContent = outputData.loc;
+    document.querySelector('#final-work').textContent = outputData.curr_work;
+    document.querySelector('#final-skills').innerHTML = outputData.skillsListHtml;
+    document.querySelector('#final-workexp').innerHTML = outputData.workExpListHtml;
+
+    generateQRCode();
+
     console.log('CV Made!')
 }
 
 function resetForm() {
-    document.querySelector('.generator-module').style.display = 'flex';
+    document.querySelector('.generator-module').style.display = 'block';
     document.querySelector('.cv-output').style.display = 'none';
 
     form = document.querySelector('#cv-form');
     form.reset();
 }
+
